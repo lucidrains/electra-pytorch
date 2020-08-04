70e8693f92b6df9634e10c29043e330c00bbbebc
Phil Wang <lucidrains@gmail.com>
2020-08-04 16:36:06 -0700

    first pass

--- README.md
+++ README.md
@@ -2,7 +2,74 @@
 
 ## Electra - Pytorch
 
-A simple wrapper for fast pretraining of language models as detailed in <a href="https://arxiv.org/abs/2003.10555">this paper</a>, for Pytorch.
+A simple wrapper for fast pretraining of language models as detailed in <a href="https://arxiv.org/abs/2003.10555">this paper</a>. The paper claims to be able to reach performances comparable to Roberta in a quarter of the compute.
+
+## Install
+
+```bash
+$ pip install electra-pytorch
+```
+
+## Usage
+
+The following example uses `reformer-pytorch`, which is available to be pip installed.
+
+```python
+import torch
+from torch import nn
+from reformer_pytorch import ReformerLM
+
+from electra_pytorch.electra_pytorch import Electra
+
+# instantiate the generator and discriminator
+
+generator = ReformerLM(
+    num_tokens = 20000,
+    dim = 512,
+    depth = 1,
+    max_seq_len = 1024
+)
+
+discriminator = ReformerLM(
+    num_tokens = 20000,
+    dim = 512,
+    depth = 2,
+    max_seq_len = 1024,
+    return_embeddings = True
+)
+
+# weight tie the token and positional embeddings of generator and discriminator
+
+generator.token_emb = discriminator.token_emb
+generator.pos_emb = discriminator.pos_emb
+
+# make sure discriminator outputs to a sigmoid (predicting replaced or original)
+
+discriminator_with_sigmoid = nn.Sequential(
+    discriminator,
+    nn.Linear(512, 1),
+    nn.Sigmoid()
+)
+
+# instantiate electra
+
+trainer = Electra(
+    generator,
+    discriminator_with_sigmoid,
+    mask_token_id = 2,
+    mask_prob = 0.15
+)
+
+# train
+
+x = torch.randint(0, 20000, (1, 1024))
+loss = trainer(x)
+loss.backward()
+
+# after much training, the discriminator should have improved
+
+torch.save(discriminator, f'./pretrained-model.pt')
+```
 
 ## Citations
 
--- electra_pytorch/electra_pytorch.py
+++ electra_pytorch/electra_pytorch.py
@@ -2,8 +2,58 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+def batched_index_select(values, indices):
+    last_dim = values.shape[-1]
+    return values.gather(1, indices[:, :, None].expand(-1, -1, last_dim))
+
 class Electra(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        generator,
+        discriminator,
+        pad_token_id = 0,
+        mask_token_id = 2,
+        mask_prob = 0.15):
         super().__init__()
-    def forward(self, x):
-        return x
+
+        self.generator = generator
+        self.discriminator = discriminator
+
+        self.mask_prob = mask_prob
+        self.pad_token_id = pad_token_id
+        self.mask_token_id = mask_token_id
+
+    def forward(self, input):
+        b, t = input.shape
+
+        mask_prob = torch.zeros_like(input).float().uniform_(0, 1)
+        mask = (mask_prob < self.mask_prob) & (input != self.pad_token_id)
+
+        masked_input = input.masked_fill(mask, self.mask_token_id)
+        gen_labels = input.masked_fill(~mask, self.pad_token_id)
+
+        print(masked_input.shape)
+        logits = self.generator(masked_input)
+
+        mlm_loss = F.cross_entropy(
+            logits.reshape(b * t, -1),
+            gen_labels.view(-1),
+            ignore_index = self.pad_token_id
+        )
+
+        mask_indices = torch.nonzero(mask, as_tuple=True)
+        sample_logits = logits[mask_indices].softmax(dim=-1)
+        sampled = torch.multinomial(sample_logits, 1)
+
+        disc_input = input.clone()
+        disc_input[mask_indices] = sampled.squeeze(-1)
+
+        disc_logits = self.discriminator(disc_input)
+        disc_labels = (input == disc_input).float()
+
+        disc_loss = F.binary_cross_entropy(
+            disc_logits.squeeze(-1),
+            disc_labels
+        )
+
+        return mlm_loss + disc_loss
