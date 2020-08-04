53f3b0e8fa09b08e94cd74f58764df306c8fe828
Phil Wang <lucidrains@gmail.com>
2020-08-04 16:40:05 -0700

    make replaced as 1 and original as 0 for discriminator

--- electra_pytorch/electra_pytorch.py
+++ electra_pytorch/electra_pytorch.py
@@ -2,10 +2,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-def batched_index_select(values, indices):
-    last_dim = values.shape[-1]
-    return values.gather(1, indices[:, :, None].expand(-1, -1, last_dim))
-
 class Electra(nn.Module):
     def __init__(
         self,
@@ -32,7 +28,6 @@ class Electra(nn.Module):
         masked_input = input.masked_fill(mask, self.mask_token_id)
         gen_labels = input.masked_fill(~mask, self.pad_token_id)
 
-        print(masked_input.shape)
         logits = self.generator(masked_input)
 
         mlm_loss = F.cross_entropy(
@@ -49,7 +44,7 @@ class Electra(nn.Module):
         disc_input[mask_indices] = sampled.squeeze(-1)
 
         disc_logits = self.discriminator(disc_input)
-        disc_labels = (input == disc_input).float()
+        disc_labels = (input != disc_input).float()
 
         disc_loss = F.binary_cross_entropy(
             disc_logits.squeeze(-1),
