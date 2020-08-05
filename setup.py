a42e83f59aff05761b514fdc9f4ce1f0ebc86558
Phil Wang <lucidrains@gmail.com>
2020-08-05 13:34:30 -0700

    add Erik to authors

--- setup.py
+++ setup.py
@@ -6,25 +6,29 @@ setup(
   version = '0.0.2',
   license='MIT',
   description = 'Electra - Pytorch',
-  author = 'Phil Wang',
-  author_email = 'lucidrains@gmail.com',
+  author = 'Erik Nijkamp, Phil Wang',
+  author_email = 'erik.nijkamp@gmail.com, lucidrains@gmail.com',
   url = 'https://github.com/lucidrains/electra-pytorch',
-  keywords = ['transformers', 'artificial intelligence', 'pretraining'],
+  keywords = [
+    'transformers',
+    'artificial intelligence',
+    'pretraining'
+  ],
   install_requires=[
-      'torch'
+    'torch'
   ],
   setup_requires=[
-      'pytest-runner'
+    'pytest-runner'
   ],
   tests_require=[
-      'pytest',
-      'reformer-pytorch'
+    'pytest',
+    'reformer-pytorch'
   ],
   classifiers=[
-      'Development Status :: 4 - Beta',
-      'Intended Audience :: Developers',
-      'Topic :: Scientific/Engineering :: Artificial Intelligence',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 3.6',
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
   ],
 )
