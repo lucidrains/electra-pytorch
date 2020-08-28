ef13c27699c2bc09bb704c27fb03268bd553aa67
Phil Wang <lucidrains@gmail.com>
2020-08-27 22:02:52 -0700

    bump for making passing num_tokens optional to Electra

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'electra-pytorch',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'Electra - Pytorch',
   author = 'Erik Nijkamp, Phil Wang',
