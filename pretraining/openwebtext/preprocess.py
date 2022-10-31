40876b1b5531178fc80890047a4b84db7b63d851
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 03:23:49 +0000

    Adding tarfile member sanitization to extractall()

--- pretraining/openwebtext/preprocess.py
+++ pretraining/openwebtext/preprocess.py
@@ -91,7 +91,26 @@ def preprocess_owt_job(tokenizer, src_dir, tmp_dir, trg_dir, job_archives, n_ten
 
         with tarfile.open(src_dir / archive) as t:
             extracted_archive = tmp_dir / f'{archive}-extracted'
-            t.extractall(extracted_archive)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, extracted_archive)
 
         for file in os.listdir(extracted_archive):
             file_path = extracted_archive / file
